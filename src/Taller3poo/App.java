3bbc2f4d5543caa23ed582f3776791e18850c021
LucasRiquelme-cde <lucas.riquelme@alumnos.ucn.cl>
2025-11-14 23:12:18 -0300

    xill

--- src/Taller3poo/App.java
+++ src/Taller3poo/App.java
@@ -5,14 +5,7 @@ package Taller3poo;
 public class App {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		Sistema s = Sistema.getSistema();
-		s.sistema();
-=======
+		System.out.println("hrno no tokes nadaaaaa");
 		System.out.println("testinggggg");
-		
-		
-		
->>>>>>> 71c73e57cce1ac1f6bd1a8de99b3a47d7022d24f
 	}
 }
