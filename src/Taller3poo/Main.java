92174e43f912cb43e488076041544db735d7be27
MatiasCollao <matias.collao@alumnos.ucn.cl>
2025-11-14 22:55:28 -0300

    fsfdsfds

--- src/Taller3poo/Main.java
+++ src/Taller3poo/Main.java
@@ -2,7 +2,11 @@ package Taller3poo;
 
 public class Main {
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
 		System.out.println("hola");
 		System.out.println("jsjsdfkSDFKSdfasdf");
+=======
+		System.out.println("ho");
+>>>>>>> Stashed changes
 	}
 }
